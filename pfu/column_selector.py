47b11a7b9a35482f0c05fd9665eb137d4e25f92e
Gašper Petelin <gapi.petelin@gmail.com>
2025-04-07 17:19:44 +0200

    Test change

--- pfu/column_selector.py
+++ pfu/column_selector.py
@@ -1,5 +1,11 @@
 from abc import ABC, abstractmethod
 from typing import List
+from pfu.data_transformers import (
+    col_prefix_target,
+    col_prefix_past_covariate,
+    col_prefix_future_covariate,
+    col_prefix_static_covariate,
+)
 
 
 class ColumnSelector(ABC):
@@ -16,3 +22,67 @@ class ColumnSelector(ABC):
 
     def get_object(self):
         return {"object": self.__class__.__name__, "params": self.get_params()}
+
+
+class CovariateSelector(ColumnSelector):
+    def __init__(
+        self,
+        select_target=False,
+        select_past_covariates=False,
+        select_future_covariates=False,
+        select_static_covariates=False,
+    ):
+        super().__init__()
+        self.select_past_covariates = select_past_covariates
+        self.select_future_covariates = select_future_covariates
+        self.select_static_covariates = select_static_covariates
+        self.select_target = select_target
+
+    def column_is_selected(self, column_name: str):
+        if column_name.startswith(f"{col_prefix_past_covariate}|") and self.select_past_covariates:
+            return True
+        if column_name.startswith(f"{col_prefix_future_covariate}|") and self.select_future_covariates:
+            return True
+        if column_name.startswith(f"{col_prefix_static_covariate}|") and self.select_static_covariates:
+            return True
+        if column_name.startswith(f"{col_prefix_target}|") and self.select_target:
+            return True
+        return False
+
+    def get_params(self):
+        return {
+            "select_past_covariates": self.select_past_covariates,
+            "select_future_covariates": self.select_future_covariates,
+            "select_static_covariates": self.select_static_covariates,
+            "select_target": self.select_target,
+        }
+
+
+class AllColumnsSelector(CovariateSelector):
+    def __init__(self):
+        super().__init__(
+            select_target=True,
+            select_past_covariates=True,
+            select_future_covariates=True,
+            select_static_covariates=True,
+        )
+
+
+class TargetColumnsSelector(CovariateSelector):
+    def __init__(self):
+        super().__init__(
+            select_target=True,
+            select_past_covariates=False,
+            select_future_covariates=False,
+            select_static_covariates=False,
+        )
+
+
+class PastCovariateColumnsSelector(CovariateSelector):
+    def __init__(self):
+        super().__init__(
+            select_target=False,
+            select_past_covariates=True,
+            select_future_covariates=False,
+            select_static_covariates=False,
+        )
--- tests/test_data_transformers.py
+++ tests/test_data_transformers.py
@@ -2,6 +2,7 @@ import pytest
 from pfu.data_transformers import NothingTransformer, TargetRollingAverages
 
 
-@pytest.mark.parametrize("transformer", [NothingTransformer(), TargetRollingAverages()])
-def test_transformers(transformer):
+@pytest.mark.parametrize("transformer_class", [NothingTransformer, TargetRollingAverages])
+def test_transformers(transformer_class):
+    t = transformer_class()
     assert 1 == 1
